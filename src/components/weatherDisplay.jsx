f6800b6daaf414d6273da6bb3fad7c34713bcc1f
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-30 23:51:39 +0530

    Update weatherDisplay.jsx

--- src/components/weatherDisplay.jsx
+++ src/components/weatherDisplay.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 const WeatherDisplay=({weather})=>{
-    const {temperature,condition}=weather;
+    const {temperature,conditions}=weather;
     const tempStyle={color: temperature > 20 ? 'red':'blue'}
     return(
         <div>
             <p>Temperature: <span style={tempStyle}>{temperature}</span></p>
-            <p>condition:{condition}</p>
+            <p>conditions:{conditions}</p>
 
 
         </div>
